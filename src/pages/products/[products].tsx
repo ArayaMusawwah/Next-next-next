4e2c2a49551121aaa32804423295670165f703bb
Araya Musawwah <miramadhan354@gmail.com>
2024-04-05 13:14:12 +0700

    Update server and static path

--- src/pages/products/[products].tsx
+++ src/pages/products/[products].tsx
@@ -7,6 +7,7 @@ import useSWR from "swr"
 
 const Product = ({ product }: { product: Product }) => {
   const { query } = useRouter()
+
   // ClientSide
   /* const { data, error, isLoading } = useSWR(
     `/api/products/${query.products}`,
@@ -22,15 +23,50 @@ const Product = ({ product }: { product: Product }) => {
           isLoading={isLoading}
         /> */}
 
-        {/* ServerSide */}
+        {/* ServerSide && Static */}
         <ProductPage product={product} />
       </Suspense>
     </div>
   )
 }
 
-export const getServerSideProps = async () => {
+// ServerSide
+/* export const getServerSideProps = async ({
+  params
+}: {
+  params: { products: string }
+}) => {
+  const getData = await fetch(
+    `http://localhost:3000/api/products/${params.products}`
+  )
+  const res = await getData.json()
+
+  return {
+    props: {
+      product: res.data
+    }
+  }
+} */
+
+//Static
+export const getStaticPaths = async (): Promise<any> => {
   const getData = await fetch("http://localhost:3000/api/products")
+  const res = await getData.json()
+
+  const paths = res.data.map((product: Product) => ({
+    params: { products: product.id }
+  }))
+
+  return { paths, fallback: false }
+}
+export const getStaticProps = async ({
+  params
+}: {
+  params: { products: string }
+}) => {
+  const getData = await fetch(
+    `http://localhost:3000/api/products/${params.products}`
+  )
   const res = await getData.json()
 
   return {
